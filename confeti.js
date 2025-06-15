0ea1bbb429b892249b9ed1fcdaedc85e21a53a46
Maribel Pereyra <mrblpryra@gmail.com>
2025-06-15 17:00:24 -0300

    arreglo

--- confeti.js
+++ confeti.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     confetti({
@@ -23,27 +23,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-=======
-window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    confetti({
-      particleCount: 100,
-      spread: 60,
-      origin: { y: 0.7 }
-    });
-  }, 800);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("confetti-btn");
-  if (btn) {
-    btn.addEventListener("click", () => {
-      confetti({
-        particleCount: 150,
-        spread: 70,
-        origin: { y: 0.6 }
-      });
-    });
-  }
-});
->>>>>>> eb9a012 (confetti)
